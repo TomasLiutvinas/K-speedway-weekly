22e804516cd676756f04f5857d1dfb68d1654e26
Tomas Liutvinas <tehdreamwalker@gmail.com>
2022-07-21 22:56:26 +0300

    inserting day, session time, laps, not yet

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ require('dotenv').config();
 // API calls
 const fetch = require('axios');
 
+// Database
+const mariadb = require('mariadb');
+
 // Constants
 const testing = true;
 const tables = ['Configurations', 'Days', 'Laps', 'Runs'];
@@ -36,32 +39,108 @@ async function fetchData(url) {
 	return data;
 }
 
-var populateDatabase = function(sessionId){
+function getWeekNumber() {
+	let d = new Date();
+	// Copy date so don't modify original
+	d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+	// Set to nearest Thursday: current date + 4 - current day number
+	// Make Sunday's day number 7
+	d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+	// Get first day of year
+	var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+	// Calculate full weeks to nearest Thursday
+	var weekNo = Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
+	// Return array of year and week number
+	return weekNo;
+}
+
+var populateDatabase = async function(sessionId){
 	let sessionData = await fetchData(`https://karts.theamazingtom.com/api/speedway/GetSessionData/${sessionId}`);
 
+	const pool = mariadb.createPool({
+		host: process.env.DB_HOST,
+		user: process.env.DB_USER,
+		password: process.env.DB_PWD,
+		database: process.env.DB_K
+	});
+
+	let connection = await pool.getConnection();
+
+	const currentDay = await connection.query(`
+		SELECT DayID 
+		FROM Days
+		WHERE DATE = '${sessionData.Date}'
+	`);
+
+	let currentDayId;
 	// Try get current day [sessionData.Date]
 	// if day not found create the day
+	if(currentDay.length > 0){
+		currentDayId = currentDay[0].DayID;
+	} else {
+		// day creation:
+			// Title[sessionData.Date]
+			// Configuration[unknown]
+			// Date[sessionData.Date]
+			// Weather[pull weather from somewhere?]
+			// Comments[null]
+			// Week[calculate week of the year]
+		let insertResult = await connection.query(`
+			INSERT INTO \`speedway\`.\`Days\` (\`Title\`, \`Date\`, \`Weather\`, \`Week\`) 
+			VALUES (
+				'${sessionData.Date}',
+				'${sessionData.Date}',
+				'UNKNOWN',
+				${getWeekNumber()}
+			);
+		`);
+		
+		// what the fuck is this, returning {id}n instead of just the ID, HELP ME PLS
+		currentDayId = insertResult.insertId.Replace('n','');
+	}
 
-	// day creation: 
-		// Title[sessionData.Date]
-		// Configuration[unknown]
-		// Date[sessionData.Date]
-		// Weather[pull weather from somewhere?]
-		// Comments[null]
-		// Week[calculate week of the year]
+	let currentSessionId;
+	let currentSession = await connection.query(`
+		SELECT SessionID 
+		FROM Sessions
+		WHERE Day = '${currentDayId}'
+			  AND
+			  Time = '${sessionData.Time}:00'
+	`);
 
 	// when day created or found
 	// try get current session
-	// if current session found delete all session data
-
-	// when session data deleted or session not found
-	// create new session
-
+	if(currentSession.length > 0){
+		currentSessionId = currentSession[0].SessionID;
+		let deleteResult = await connection.query(`
+			DELETE FROM \`speedway\`.\`Sessions\`
+			WHERE Day = ${currentDayId}
+				  AND
+				  Time = '${sessionData.Time}:00'
+		`);
+		// if current session found delete ALL session data
+		console.log(deleteResult);
+	}
+	
 	// session creation:
-		// Day[sessionData.Date{ref}]
-		// Time[sessionData.Time]
-		// Type[determine type, or upgrade API to return type]
-		// SpeedwaySessionID[sessionId]
+	// Day[sessionData.Date{ref}]
+	// Time[sessionData.Time]
+	// Type[determine type, or upgrade API to return type]
+	// SpeedwaySessionID[sessionId]
+
+	// TODO Resolve Simple/Other Types
+	let insertResult = await connection.query(`
+		INSERT INTO \`speedway\`.\`Sessions\` (\`Day\`, \`Time\`, \`Type\`, \`SpeedwaySessionID\`) 
+		VALUES (
+			 ${currentDayId},
+			'${sessionData.Time}:00',
+			'SIMPLE',
+			'${sessionId}'
+		);
+	`);
+
+	currentSessionId = insertResult.insertId;
+	console.log(`Created ${currentSessionId}`);	
 
 	// when session created
 	// bulk add laps
@@ -73,16 +152,20 @@ var populateDatabase = function(sessionId){
 		// Position[sessionData.LapTimes[...].data[index]] Only if sesion type is race
 		// Kart[sessionData.LapTimes[i].label for all entries in a set]
 		// Driver[unknown]
+
+	connection.end();
+	console.log('closed connection');
 }
 
 var init = async function (){
 	if (testing) {
 		let sessionId = await fetchData('https://karts.theamazingtom.com/api/speedway/GetCurrentSessionId');
-		console.log(sessionData.LapTimes[0].data);
-		populateDatabase(sessionId);
+		await populateDatabase(sessionId);
+		return "";
 	}
 
 	while (true && !testing) {
+		console.log('polling');
 		setTimeout(() => {
 			let res = fetchData(`https://karts.theamazingtom.com/api/speedway/GetSessionData/`);
 
